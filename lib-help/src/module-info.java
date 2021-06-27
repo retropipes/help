80af06a8d847fc6657b382a427fd464c05e594d0
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-06-27 14:17:58 -0400

    Format code.

--- lib-help/src/module-info.java
+++ lib-help/src/module-info.java
@@ -1,5 +1,5 @@
 module com.puttysoftware.help {
-	requires transitive java.desktop;
-	
-	exports com.puttysoftware.help;
+    requires transitive java.desktop;
+
+    exports com.puttysoftware.help;
 }
